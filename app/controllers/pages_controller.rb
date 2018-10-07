13420b8fa81b7b6cbbd2015fa4848a457eb7a4c6
Bastien Gason <gasonbastien@gmail.com>
2018-10-08 02:54:56 +0700

    add other pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def contact
+  end
+
+  def events
+  end
+
+  def blog
+  end
+
+  def ressources
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#home'
+
+  # Pages routes
+  get '/about', to: 'pages#about'
+  get '/blog', to: 'pages#blog'
+  get '/challenges', to: 'pages#challenges'
+  get '/contact', to: 'pages#contact'
+  get '/events', to: 'pages#events'
+  get '/ressources', to: 'pages#ressources'
+  get '/thankyou', to: 'pages#thankyou'
+
 end
