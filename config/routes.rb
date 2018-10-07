3c17780388a6c4a96d4c4bf3dab551599d4dbcf6
Bastien Gason <gasonbastien@gmail.com>
2018-10-08 05:10:14 +0700

    typo routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
   # Pages routes
   get '/about', to: 'pages#about'
   get '/blog', to: 'pages#blog'
+
   get '/challenges', to: 'pages#challenges'
+    # Challenges routes
     get '/challenges/15-days-css', to: 'challenges#fifteen_day_css'
     get '/challenges/css-grid', to: 'challenges#css_grid'
     get '/challenges/animation', to: 'challenges#animation'
+
   get '/contact', to: 'pages#contact'
   get '/events', to: 'pages#events'
   get '/ressources', to: 'pages#ressources'
